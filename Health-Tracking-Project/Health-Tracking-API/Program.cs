c68b8e3da24624d64416dbbb3b2a5b75cf137fd9
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2023-08-23 01:58:10 +0530

    Changed the solution directory and name

--- Health-Tracking-Project/Health-Tracking-API/Program.cs
+++ Health-Tracking-Project/Health-Tracking-API/Program.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Health_Tracking_App
+namespace Health_Tracking_API
 {
     public class Program
     {
--- Health-Tracking-Project/Health-Tracking-API/Startup.cs
+++ Health-Tracking-Project/Health-Tracking-API/Startup.cs
@@ -12,7 +12,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Health_Tracking_App
+namespace Health_Tracking_API
 {
     public class Startup
     {
@@ -30,7 +30,7 @@ namespace Health_Tracking_App
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Health_Tracking_App", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Health_Tracking_API", Version = "v1" });
             });
         }
 
@@ -41,7 +41,7 @@ namespace Health_Tracking_App
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Health_Tracking_App v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Health_Tracking_API v1"));
             }
 
             app.UseHttpsRedirection();
