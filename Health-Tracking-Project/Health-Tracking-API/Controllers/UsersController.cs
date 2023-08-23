d6e94660e8cc564ec40daec3e28e02a2389025e9
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2023-08-24 00:28:22 +0530

    Created use controller and some loose apis

---
+++ Health-Tracking-Project/Health-Tracking-API/Controllers/UsersController.cs
@@ -0,0 +1,60 @@
+﻿using Health_Tracking_DataService.Data;
+using Health_Tracking_Entities.DbSet;
+using Health_Tracking_Entities.Dtos.Incomming;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+
+namespace Health_Tracking_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private AppDbContext _context;
+        public UsersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        //GetAll
+        [HttpGet]
+        public IActionResult GetUsers() 
+        {
+            var users = _context.Users.Where(x => x.Status == 1).ToList();
+            return Ok(users);
+        }
+
+
+        //Post
+
+        [HttpPost]
+        public IActionResult AddUser(UserDto user)
+        {
+            var _user = new User();
+
+            _user.FirstName = user.FirstName;
+            _user.LastName = user.LastName;
+            _user.Email = user.Email;
+            _user.Status = 1;
+            _user.Phone = user.Phone;
+            _user.DateOfBirth = Convert.ToDateTime(user.DateOfBirth);
+           _user.Country = user.Country;
+
+            _context.Users.Add(_user);
+            _context.SaveChanges();
+
+            //will return 201 in future
+            return Ok();
+        }
+
+        //Get
+        [HttpGet]
+        [Route("GetUser")]
+        public IActionResult GetUser(Guid id)
+        {
+            var user = _context.Users.FirstOrDefault(x => x.Id == id);
+            return Ok(user);
+        }
+    }
+}
---
+++ Health-Tracking-Project/Health-Tracking-Entities/Dtos/Incomming/UserDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Health_Tracking_Entities.Dtos.Incomming
+{
+    public class UserDto
+    {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public String DateOfBirth { get; set; }
+        public string Country { get; set; }
+
+    }
+}
