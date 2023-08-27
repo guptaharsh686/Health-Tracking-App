dd354b3265dae8cc8561662251853803d75b5997
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2023-08-27 14:54:13 +0530

    Jwt Token : Created REgisteration DTOs

---
+++ Health-Tracking-Project/Health-Tracking-Authentication/Models/DTO/Outgoing/AuthResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Health_Tracking_Authentication.Models.DTO.Outgoing
+{
+    public class AuthResult
+    {
+        public string Token { get; set; }
+        public bool Success { get; set; }
+        public List<string> Errors { get; set; }
+    }
+}
---
+++ Health-Tracking-Project/Health-Tracking-Authentication/Models/DTO/Outgoing/UserRegisterationResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Health_Tracking_Authentication.Models.DTO.Outgoing
+{
+    public class UserRegisterationResponse : AuthResult
+    {
+    }
+}
